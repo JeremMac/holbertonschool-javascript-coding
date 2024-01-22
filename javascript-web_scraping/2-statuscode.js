509c0014e7fded304353d6687e8aad53f7ff3915
JeremMac <macal.jerem@gmail.com>
2024-01-22 13:34:14 +0100

    Modified if condition.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, (error, response) => {
+  if (error) {
+    return;
+  }
   console.log('code:', response.statusCode);
 });
