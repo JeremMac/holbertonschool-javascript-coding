2bad1a91ec1792511f55d8dd7eb38b1f628edb4b
JeremMac <macal.jerem@gmail.com>
2024-01-22 14:57:51 +0100

    Changed entire code.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,6 @@
 
 const url = process.argv[2];
 const request = require('request');
-request(url, (error, response) => {
-  if (error) {
-    console.error('code:', error);
-    return;
-  }
+request.get(url).on('response', function (response) {
   console.log('code:', response.statusCode);
 });
