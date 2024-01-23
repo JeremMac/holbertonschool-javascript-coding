90abb82968769dd3c9fc542ba7006608e63137ab
JeremMac <macal.jerem@gmail.com>
2024-01-23 11:28:59 +0100

    Changed code by adding for loop.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,20 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = 18;
+let count = 0;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
   } else {
-    const filmsData = JSON.parse(body);
-    const filmsWithWedgeAntilles = filmsData.results.filter(film => film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`));
-    console.log(filmsWithWedgeAntilles.length);
+    const filmsData = JSON.parse(body).results;
+    for (const result of filmsData) {
+      for (const characters of result.characters) {
+        if (characters.includes(characterId)) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
   }
 });
