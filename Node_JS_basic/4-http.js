2c500fc88b5bd2e58b293baf49e98c7eb3b14d27
JeremMac <macal.jerem@gmail.com>
2024-02-02 09:46:49 +0100

    Changed code.

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
+// Create an HTTP server
 const app = http.createServer((req, res) => {
+  // Set the response header
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response
   res.end('Hello Holberton School!\n');
 });
 
-app.listen(1245);
+// Set the server to listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
 
+// Export the app variable
 module.exports = app;
